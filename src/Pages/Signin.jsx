8e19c450e57090d920f6fc0422afe3056e6e8060
Karthik <163863077+Karthiksaran1610@users.noreply.github.com>
2024-08-16 19:51:36 +0530

    Update Signin.jsx

--- src/Pages/Signin.jsx
+++ src/Pages/Signin.jsx
@@ -30,7 +30,7 @@ const Signin = () => {
     try {
       dispatch(signInStart());
       const response = await fetch(
-        "http://localhost:5000/api/auth/login-user",
+        "https://backendcapstone10.onrender.com/api/auth/login-user",
         {
           method: "POST",
           headers: {
